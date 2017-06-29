dceec86bcc17b9db1bdbc5b3343b6e716e23a527
koffater123 <koffater123@hotmail.com>
2017-06-29 10:57:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 $text = file_get_contents('http://tts.railway.co.th/srttts/view');
-echo (stristr ($text, 'กรุงเทพ')) ? 'found' : 'not found';
+echo (stristr ($text, 'กรุงเทพ1')) ? 'found' : 'not found';
 ?>
