87c6d1d540207e0b5da358b3e1c18283cce2b259
koffater123 <koffater123@hotmail.com>
2017-06-29 21:29:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,58 @@
 <?php
-$text = file_get_contents('http://tts.railway.co.th/srttts/view');
-echo (stristr ($text, 'กรุงเทพ1')) ? 'found' : 'not found';
-?>
+ include('simple_html_dom.php');
+ $html = file_get_html('http://tts.railway.co.th/srttts/view');
+ foreach($html->find('table tr td') as $e){
+    $arr[] = trim($e->innertext);
+  }
+  for ($i=12;$i<sizeof($arr)-9;$i+=9)
+  {
+	  print_r($arr[$i]);
+	  echo "\r\n";
+  }
+  print_r($arr[12]);
+  print_r($arr[13]);
+  print_r($arr[14]);
+  print_r($arr[15]);
+//print_r($arr);
+$access_token = 'EZhc3qkbHr8CzNimKi8y3eZbY+C897upwPu/0Np+4I1XhliJCxBVY9oCjPgAB9SXA69lnCGL/O7Qw+81fWTeJJyeQShqZDNXBjFU1VGPJ0XeIGXVS7DFD197JIBBcyUzfMuVj5gjKqm2F2amX+mj/AdB04t89/1O/w1cDnyilFU=';
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
+		}
+	}
+}
+echo "OK";
+ ?>
