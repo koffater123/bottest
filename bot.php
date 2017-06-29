faf6d9c8788d3afe0dbb53e3fc34c8f1eb475686
koffater123 <koffater123@hotmail.com>
2017-06-29 21:57:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -36,8 +36,8 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				//'messages' => [$messages],
-				'messages' => [$arr[13]],
+				'messages' => [$messages],
+				
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
